377a6f5124d0275b6faec7da4e9cd023b8a82eb1
hyde <hyde.song@yahoo.com>
2023-06-24 04:54:45 +0800

    fic

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -14,8 +14,6 @@ export function Main({ name = "Extension" }) {
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> {name}!
       </h1>
       <input onChange={(e) => setData(e.target.value)} value={data} />
-
-      <a href="https://docs.plasmo.com">READ THE DOCS!</a>
     </div>
   )
 }
